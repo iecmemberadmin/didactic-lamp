986a2dd7d88058c9e5bdf84e366426e2212803f2
jgabaja <jgabaja@gmail.com>
2018-10-08 22:14:47 +0800

    wip

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// eslint-disable-next-line
+import { Card, CardText, CardBody, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import iec from '../../Assets/Images/ieclub.png';
+
+const Login = () => {
+  return( 
+    <div className='container'>
+      <div className='centered'><h4 className='text-danger'><img alt='IECLUBLOGO' src={iec} height='5%' width='5%' /> Inside The Club</h4></div>
+      <div>
+        <Card id='login'>
+          <CardBody>
+            <Form>
+              <FormGroup>
+                <Label for="studNum">Student Number</Label>
+                <Input type="text" name="studNum" id="studNum" placeholder="2015-12345" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input type="password" name="password" id="password" />
+              </FormGroup>
+              <div className='centered'><Button outline color='danger' href='/dashboard'>Login</Button></div>
+            </Form>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Components/NavMenu/NavMenu.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+import iec from '../../Assets/Images/ieclub.png';
+
+
+class NavMenu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="danger" dark expand="md">
+          <NavbarBrand href="/"><img alt='IECLUBLOGO' src={iec} height='10%' width='10%'/> Inside The Club</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+};
+
+export default NavMenu;
